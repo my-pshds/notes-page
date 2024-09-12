3cda38eb6301df9d7fbe4dd55dc3bd467e9c00c0
Lyan Ketin Kanon Runningwind Roffi Roffe <roffi.peng@foxmail.com>
2024-09-12 21:23:11 +0800

    恢复默认搜索

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -64,12 +64,31 @@ const config = {
         // Public API key: it is safe to commit it
         apiKey: 'e935ddad5144dd2f8243e5cc79658cd1',
   
-        indexName: 'academic-pengxianzhe',
+        indexName: 'academic-pengxianzhe-66cc1eefaa767b0036e8d745',
   
         // Optional: see doc section below
-        contextualSearch: false,
-        
+        contextualSearch: true,
+  
+        // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+        externalUrlRegex: 'external\\.com|domain\\.com',
+  
+        // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
+        replaceSearchResultPathname: {
+          from: '/docs/', // or as RegExp: /\/docs\//
+          to: '/',
         },
+  
+        // Optional: Algolia search parameters
+        searchParameters: {},
+  
+        // Optional: path for search page that enabled by default (`false` to disable it)
+        searchPagePath: 'search',
+  
+        // Optional: whether the insights feature is enabled or not on Docsearch (`false` by default)
+        insights: false,
+  
+        //... other Algolia params
+      },
 
       colorMode: {
         defaultMode: 'light',
